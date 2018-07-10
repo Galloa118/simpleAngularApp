fb58fc679fb98c5767ee0d0733c6d9ce6635f95b
Galloa118 <pewallin0527@gmail.com>
2018-07-10 15:56:24 +0300

    add displaying heroes using HEROES

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
@@ -7,11 +8,13 @@ import { Hero } from '../hero';
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
+  
+  heroes = HEROES;
+  selectedHero: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'windstorm'
-  };
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   
   constructor() { }
 
